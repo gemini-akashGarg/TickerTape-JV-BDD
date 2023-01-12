cde33034680ef6941bcd4092bec6b3428598b8e7
Pawan.Deep <pawan.deep@geminisolutions.com>
2023-01-12 19:09:02 +0530

    For Lambda with Headless chrome and DesiredCapabilities

--- src/main/java/com/qa/TickerTape/StepDefination/TickerTapeHook.java
+++ src/main/java/com/qa/TickerTape/StepDefination/TickerTapeHook.java
@@ -1,18 +1,15 @@
 package com.qa.TickerTape.StepDefination;
 
-import com.gemini.generic.exception.GemException;
-import com.gemini.generic.reporting.GemTestReporter;
 import com.gemini.generic.ui.utils.DriverAction;
 import com.gemini.generic.ui.utils.DriverManager;
 import com.gemini.generic.utils.GemJarGlobalVar;
 import com.gemini.generic.utils.GemJarUtils;
-import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class TickerTapeHook {
     @Before
-    public void start() throws GemException {
+    public void start() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--window-size=1920,1080");
         options.addArguments("--disable-gpu");
@@ -28,12 +25,4 @@ public class TickerTapeHook {
         DriverAction.setPageLoadTimeOut(Long.parseLong(GemJarGlobalVar.pageTimeout));
         DriverAction.setScriptTimeOut(Long.parseLong(GemJarGlobalVar.scriptTimeout));
     }
-//    @AfterAll
-//    public static void after_all() {
-//        try {
-//            GemTestReporter.endSuite(System.getProperty("java.io.tmpdir"));
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
 }
